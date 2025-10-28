a91efd17fd414cc40a3c9d64734d9f84d233b667
1h4t39009le <1h4t39009le@gmail.com>
2025-10-28 23:37:10 +0300

    Back to basics

--- vulkan_learning/vulkan_learning.cpp
+++ vulkan_learning/vulkan_learning.cpp
@@ -2,21 +2,35 @@
 //
 
 #include "vulkan_learning.h"
+#include "vulkan/vulkan_raii.hpp"
+
+/*
+typedef struct VkApplicationInfo {
+    VkStructureType    sType;
+    const void*        pNext;
+    const char*        pApplicationName;
+    uint32_t           applicationVersion;
+    const char*        pEngineName;
+    uint32_t           engineVersion;
+    uint32_t           apiVersion;
+} VkApplicationInfo;
+*/
 
 int main() {
+	auto extensions = vk::enumerateInstanceExtensionProperties();
+    vk::ApplicationInfo app_info{
+        "Name", 
+        vk::makeVersion(1,0,0), 
+        "EngineName", 
+        vk::makeVersion(1,0,0), 
+        vk::ApiVersion10
+    };
+    vk::InstanceCreateInfo info{{},&app_info};
+	vk::raii::Context ctx{};
+	vk::raii::Instance instance{ctx, info};
 
-    glfwInit();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    
-    GLFWwindow* window = glfwCreateWindow(800, 600, "METANIT.COM", nullptr, nullptr);
 
-    while (!glfwWindowShouldClose(window)) {
-        glfwWaitEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    
-    glfwTerminate();
+    std::cout << app_info.pApplicationName << '\n';
+    std::cout << app_info.applicationVersion << '\n';  // 4194304 или 10000000000000000000000
 }
--- vulkan_learning/vulkan_learning.h
+++ vulkan_learning/vulkan_learning.h
@@ -1,4 +1,3 @@
 ﻿#pragma once
 #include <iostream>
 #include <vulkan/vulkan.hpp>
-#include <GLFW/glfw3.h>
